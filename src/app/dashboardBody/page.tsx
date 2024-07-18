4a164720c80f3d4db2e446c881dd0acc2c725761
Dvasrn <dawaasurenbazarragcha4@gamil.com>
2024-07-18 18:34:52 +0800

    srcX

--- src/app/dashboardBody/page.tsx
+++ src/app/dashboardBody/page.tsx
@@ -68,26 +68,26 @@ export default function DashboardBody({
             </div>
           </div>
         </div>
-
-        <div className={style.header}>
-          <div>
-            <img src="menu.png" className={style.menu} />
-          </div>
-          <div className={style.imgFlex}>
-            <img src="darkMode.png" className={style.menu} />
-            <img src="notifications.png" className={style.menu} />
-          </div>
-          <div className={style.profileFlex}>
-            <div className={style.profileName}>admin</div>
-            <div className={style.profileImgBorder}>
-              <img src="profile.png" className={style.profileImg} />
+        <div>
+          <div className={style.header}>
+            <div>
+              <img src="menu.png" className={style.menu} />
+            </div>
+            <div className={style.imgFlex}>
+              <img src="darkMode.png" className={style.menu} />
+              <img src="notifications.png" className={style.menu} />
+            </div>
+            <div className={style.profileFlex}>
+              <div className={style.profileName}>admin</div>
+              <div className={style.profileImgBorder}>
+                <img src="profile.png" className={style.profileImg} />
+              </div>
             </div>
           </div>
         </div>
-
         <div className={style.mainBody}>{children}</div>
-    </div>
       </div>
+    </div>
   );
 }
 
